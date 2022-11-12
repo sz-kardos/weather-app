65990f361fd5191ae5e1d9914d3757cefafa18ad
Szabina Kardos <82178685+sz-kardos@users.noreply.github.com>
2022-11-12 22:26:37 +0100

    "bug fixed"

--- src/App.js
+++ src/App.js
@@ -1,89 +1,68 @@
-
-import { useState, useEffect } from "react";
-import Clear from "./assets/clear.jpg";
-import Cloudy from "./assets/cloudy.jpg";
-import Overcast from "./assets/overcast.jpg";
-import Rainy from "./assets/rainy.jpg";
-import Snow from "./assets/snow.jpg";
-import { AiOutlineSearch } from 'react-icons/ai';
+import axios from 'axios';
+import React, {useState} from 'react';
 
 
 function App() {
-  const [place, setPlace] = useState("London");
+
+  const [place, setPlace] = useState('');
   const [placeInfo, setPlaceInfo] = useState({});
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${place}&units=metric&appid=b0254f57125b747e14e5f553029e6747`
 
-  useEffect(() => {
-    handleFetch();
-  }, []);
 
-  const handleFetch = () => {
-    fetch(
-      `http://api.weatherapi.com/v1/forecast.json?key=abf00a491ea041d388d155613222906&q=${place}&days=1&aqi=no&alerts=no`
-    )
-      .then((response) => response.json())
-      .then((data) =>
-        setPlaceInfo({
-          name: data.location.name,
-          country: data.location.country,
-          celsius: {
-            current: data.current.temp_c,
-            high: data.forecast.forecastday[0].day.maxtemp_c,
-            low: data.forecast.forecastday[0].day.mintemp_c
-          },
-          condition: data.current.condition.text
-        })
-      );
-
-    setPlace("");
-  };
-
-  console.log(`abf00a491ea041d388d155613222906`)
-
-  return (
-    <div
-      className="h-screen w-screen bg-no-repeat bg-center bg-cover relative font-sans bg-white duration-100 overflow-hidden "
-      style={
-        placeInfo.condition?.toLowerCase() === "clear" ||
-          placeInfo.condition?.toLowerCase() === "sunny"
-          ? { backgroundImage: `url(${Clear})` }
-          : placeInfo.condition?.includes("cloudy")
-            ? { backgroundImage: `url(${Cloudy})` }
-            : placeInfo.condition?.toLowerCase().includes("rainy")
-              ? { backgroundImage: `url(${Rainy})` }
-              : placeInfo.condition?.toLowerCase().includes("snow")
-                ? { backgroundImage: `url(${Snow})` }
-                : { backgroundImage: `url(${Overcast})` }
-      }
-    >
-      <div className="px-2 flex items-center  justify-center flex-row absolute border-1 bg-white border shadow-md rounded-full mt-4 w-1/4 mr-auto ml-auto bg-transparent overflow-hidden right-2  ">
-        <input
-          className="rounded-l-sm w-full py-2 px-6 text-white leading-tight focus:outline-none bg-transparent flex"
-          type="text"
-          value={place}
-          onChange={(e) => setPlace(e.target.value)}
-
-        />
-        <AiOutlineSearch
-          onClick={handleFetch}
-          fontSize="large"
-          className="bg-transparent text-white rounded-full p-2 hover:bg-red-400 focus:outline-none w-12 h-12 flex items-center justify-center"
-        />
-      </div>
-      <div className="mt-3 w-1/4 mr-auto ml-auto text-white absolute flex items-center justify-center flex-row left-2.5">
-        <div className="absolute flex items-center justify-center flex-row left-12 top-5 text-lg w-max">
-          <h1 className="text-5xl m-2 py-5  w-max relative">{placeInfo.celsius?.current}° C</h1>
-          <div className="ml-6 w-1/4 mr-auto top-8 mb-8 relative ">
-            <h1>{placeInfo.condition}</h1>
-            <h1>{placeInfo.celsius?.high}° C</h1>
-            <h1>{placeInfo.celsius?.low}° C</h1>
-          </div>
-        </div>
-        <h2 className="text-3xl pl-4 flex items-center justify-center relative m-4  top-40">
-          {placeInfo.name}, {placeInfo.country}
-        </h2>
-      </div>
-    </div>
-  );
+ const location = (event) => {
+  if (event.key === 'Enter') {
+    axios.get(url).then((response) => {
+      setPlaceInfo(response.data)
+      console.log(response.data)
+      
+    })
+    setPlace('')
+  }
 }
 
+  return ( 
+    <div className="app" >
+    <div className="search-box">
+      <input
+      value={place}
+      onChange={event => setPlace(event.target.value)}
+      onKeyPress={location}
+      placeholder='Enter Location'
+      type="text" 
+     />
+      
+    </div>
+
+    <div className="container">
+      <div className="top">
+        <div className="location">
+          <p>{placeInfo.name} </p> 
+        </div>
+        <div className="temp">
+        {placeInfo.main ? <h1>{placeInfo.main.temp.toFixed()}°C</h1> : null}
+        </div>
+        <div className="description">
+        {placeInfo.weather ? <p>{placeInfo.weather[0].main}</p> : null}
+        </div>
+      </div>
+
+    <div className="bottom">
+    <div className="feels">
+              {placeInfo.main ? <p className='bold'>{placeInfo.main.temp_max.toFixed()}°C</p> : null}
+              <p>Max Temp</p>
+            </div>
+            <div className="humidity">
+              {placeInfo.main ? <p className='bold'>{placeInfo.main.humidity}%</p> : null}
+              <p>Humidity</p>
+            </div>
+            <div className="wind">
+              {placeInfo.wind ? <p className='bold'>{placeInfo.wind.speed.toFixed()} MPH</p> : null}
+              <p>Wind Speed</p>
+      </div>
+    </div>
+ </div>
+    </div> 
+ );
+} 
+ 
 export default App;
